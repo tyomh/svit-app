76e58278f967dc6f2c3d4a7ebdce168642d68031
happydudu <happydudu1@gmail.com>
2019-04-13 11:24:16 -0700

    l6update1

--- src/main/java/com/svit/java/l6/FactoryMethodDemo.java
+++ src/main/java/com/svit/java/l6/FactoryMethodDemo.java
@@ -1,11 +1,11 @@
 package com.svit.java.l6;
-
 /**
  * 
- * @author sv-it
+ * Code is for study and personal use purpose, not for commercial use.
+ * 
+ * @author sv-it.org
  *
  */
-
 interface Shape {
 	void draw();
 	String getDesc();
@@ -50,28 +50,6 @@ class Circle implements Shape {
 }
 
 class ShapeFactory {
-
-	// use getShape method to get object of type shape
-	public Shape getShape(String shapeType) {
-		if (shapeType == null) {
-			return null;
-		}
-		if (shapeType.equalsIgnoreCase("CIRCLE")) {
-			return new Circle();
-
-		} else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
-			return new Rectangle();
-
-		} else if (shapeType.equalsIgnoreCase("SQUARE")) {
-			return new Square();
-		}
-
-		return null;
-	}
-}
-
-
-class SizeFactory {
 
 	// use getShape method to get object of type shape
 	public Shape getShape(String shapeType) {
